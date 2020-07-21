cf9f3c91d4e07df38d73b8614c2c64f8e345a992
Will Spencer <will.e.spencer@gmail.com>
2020-07-21 12:22:54 -0400

    Test put request

---
+++ frontend/scheduleaf/test/album.dart
@@ -0,0 +1,13 @@
+class Album {
+  final int id;
+  final String title;
+
+  Album({this.id, this.title});
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      id: json['id'],
+      title: json['title'],
+    );
+  }
+}
