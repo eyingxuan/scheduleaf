966e65d060e3095c7c50e2a9cf8dbd17b334c673
Will Spencer <will.e.spencer@gmail.com>
2020-07-21 12:33:34 -0400

    Refactor and clean up starter code

--- frontend/scheduleaf/lib/main.dart
+++ frontend/scheduleaf/lib/main.dart
@@ -1,52 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-
-// import 'album.dart';
+import "test_response.dart";
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class Album {
-  final int id;
-  final String msg;
-  final String name;
-
-  Album({this.id, this.msg, this.name});
-
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
-      id: json['id'],
-      msg: json['msg'],
-      name: json['name'],
     );
   }
 }
@@ -61,27 +31,27 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Future<Album> futureAlbum;
+  Future<TestResponse> responseObject;
 
-  Future<Album> createAlbum(String title) async {
+  Future<TestResponse> createResponse(String msg, String name) async {
     final http.Response response = await http.put(
       'http://10.0.2.2:8000/sched/1',
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
       body: jsonEncode(<String, String>{
-        'msg': title,
-        'name': "will",
+        'msg': msg,
+        'name': name,
       }),
     );
     if (response.statusCode == 200) {
-      // If the server did return a 201 CREATED response,
+      // If the server did return a 200 OK response,
       // then parse the JSON.
-      return Album.fromJson(json.decode(response.body));
+      return TestResponse.fromJson(json.decode(response.body));
     } else {
-      // If the server did not return a 201 CREATED response,
+      // If the server did not return a 200 OK response,
       // then throw an exception.
-      throw Exception('Failed to load album');
+      throw Exception('Failed to update response');
     }
   }
 
@@ -109,8 +79,8 @@ class _MyHomePageState extends State<MyHomePage> {
             FlatButton(
               onPressed: () {
                 setState(() {
-                  futureAlbum = createAlbum("testusername");
-                  print("hi");
+                  responseObject = createResponse("test2", "will2");
+                  print("response received");
                 });
               },
               child: Text(
